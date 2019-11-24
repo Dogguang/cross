47de2c39bf4ec0844e23020a5d720373e116ee82
dogguang <457444894@qq.com>
2019-11-24 17:40:40 +0800

    test

---
+++ build/precache-manifest.6c294b2364a41f0f77ff118d5ef49761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3d3a183b96f7b2b0709d041f17496b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0f3debd0125a5513195",
+    "url": "./static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "./static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "c0f3debd0125a5513195",
+    "url": "./static/js/main.1d3d50ce.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  }
+]);
